e60034a4f04a9c10d483ad41e48c01267e7d0329
José Campelo <josecampelojr@gmail.com>
2025-09-13 07:08:07 -0300

    chore: Configura a conversão de Enum para string no DbContext

--- ControleDespesas/Data/AppDbContext.cs
+++ ControleDespesas/Data/AppDbContext.cs
@@ -18,4 +18,18 @@ public class AppDbContext : DbContext
     /// Mapeia a entidade 'Transacao' para uma tabela no banco de dados.
     /// </summary>
     public DbSet<Transacao> Transacoes { get; set; }
+
+    /// <summary>
+    /// Configura o modelo de dados e o mapeamento para o banco de dados.
+    /// Este método é chamado pelo Entity Framework Core durante a inicialização do contexto.
+    /// </summary>
+    /// <param name="modelBuilder">O construtor que está sendo usado para construir o modelo para este contexto.</param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Transacao>()
+            .Property(transacao => transacao.Tipo)
+            .HasConversion<string>();
+    }
 }
